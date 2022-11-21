4579408b1aa0706fc20fb1a93e8b4036083c936c
Oleg NN <zvoprosno@gmail.con>
2022-11-21 15:27:52 +0300

    Калькулятор

---
+++ src/com/company/IntsCalculator.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class IntsCalculator implements Ints{
+    Calculator calc = new Calculator();
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        return (int) calc.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM)
+                .result();
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        return (int) calc.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result();
+    }
+
+    @Override
+    public int pow(int arg0, int arg1) {
+        return (int) calc.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.POW)
+                .result();
+    }
+
+    @Override
+    public int sub(int arg0, int arg1) {
+        return (int) calc.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUB)
+                .result();
+    }
+
+    @Override
+    public int div(int arg0, int arg1) {
+        return (int) calc.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.DIV)
+                .result();
+    }
+}
